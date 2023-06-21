5c2639c4c7bb3e8aa000031b0901348e09a92be1
michcastrillo63 <michcastrillo63@gmail.com>
2023-06-20 18:12:08 -0600

    Módulo 3 Episodio 15

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,13 +1,15 @@
-<!DOCTYPE html>
-<title>My blog</title>
-<link rel="stylesheet" href="/app.css">
-<body>
-    <?php foreach ($posts as $post) : ?>
+<x-layout>
+    @foreach ($posts as $post)
     <article>
-       <h1><a href="/posts/<?= $post->slug; ?>"><?= $post->title; ?></a></h1>
-       <div>
-            <?= $post->body; ?>
+        <h1>
+            <a href="/posts/{{$post->slug; }}">
+                {{ $post->title; }}
+            </a>
+        </h1>
+      
+        <div>
+            {{ $post->excerpt; }}
         </div>
     </article>
-    <?php endforeach; ?>
-</body>
+    @endforeach
+</x-layout>
